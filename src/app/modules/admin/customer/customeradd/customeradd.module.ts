7d76d10b6149076e07426b21828ceb6f35d53a09
Dogan Gunes <dogangns.98@gmail.com>
2023-07-26 09:17:34 +0300

    .

--- src/app/modules/admin/customer/customeradd/customeradd.module.ts
+++ src/app/modules/admin/customer/customeradd/customeradd.module.ts
@@ -10,6 +10,13 @@ import { FuseCardModule } from '@fuse/components/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { SharedModule } from 'app/shared/shared.module';
 
 const root: Route[] = [
     {
@@ -32,6 +39,20 @@ const root: Route[] = [
         MatSelectModule,
         MatIconModule,
         MatCheckboxModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatMenuModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatRippleModule,
+        MatSortModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+        MatTooltipModule,
+        SharedModule,
         RouterModule.forChild(root),
      
     ],
