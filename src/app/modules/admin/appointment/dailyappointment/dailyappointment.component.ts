1fb59867b38b7296fb8cba912404faa33bf3d074
Dogan Gunes <dogangns.98@gmail.com>
2024-05-07 00:56:12 +0300

    .

--- src/app/modules/admin/appointment/dailyappointment/dailyappointment.component.ts
+++ src/app/modules/admin/appointment/dailyappointment/dailyappointment.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { DailyAppointmentListDto } from './models/dailyappointmentlistdto';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-dailyappointment',
@@ -6,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dailyappointment.component.css']
 })
 export class DailyappointmentComponent implements OnInit {
+
+  displayedColumns: string[] = ['date', 'customerPatientName', 'services', 'services', 'actions'];
+
+  isUpdateButtonActive: boolean;
+  @ViewChild('paginator') paginator: MatPaginator;
+  dailyappointment: DailyAppointmentListDto[] = [];
+  dataSource = new MatTableDataSource<DailyAppointmentListDto>(this.dailyappointment);
+
 
   constructor() { }
 
