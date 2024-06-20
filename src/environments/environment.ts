88e81cdad3d207d34de7ce73cc0959cc78e70c31
Dogan Gunes <dogangns.98@gmail.com>
2024-06-21 02:00:43 +0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/services/',
+    // apiUrl: 'http://localhost:5000/services/',
+    apiUrl: 'http://46.101.120.6:5000/services/',
     appName: 'vetsystems',
     env: 'dev',
     IsApiConnect : true
