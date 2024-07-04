2857992d445094b4dab436a4a9a265d4b017ceae
Dogan Gunes <dogangns.98@gmail.com>
2024-07-04 18:30:26 +0300

    .

--- src/app/modules/admin/commonscreen/print-template-selected/print-template-selected.component.ts
+++ src/app/modules/admin/commonscreen/print-template-selected/print-template-selected.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { TranslocoService } from '@ngneat/transloco';
+import { GeneralService } from 'app/core/services/general/general.service';
+import { LogService } from 'app/core/services/logs/logService.service';
+import { SweetalertType } from 'app/modules/bases/enums/sweetalerttype.enum';
+import { SweetAlertDto } from 'app/modules/bases/models/SweetAlertDto';
 
 @Component({
   selector: 'app-print-template-selected',
@@ -7,9 +14,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrintTemplateSelectedComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _dialogRef: MatDialogRef<any>,
+    private _translocoService: TranslocoService,
+    private _logService: LogService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  closeDialog(): void {
+    this._dialogRef.close({ status: null });
+  }
+
+  showSweetAlert(type: string, text: string): void {
+    if (type === 'success') {
+      const sweetAlertDto = new SweetAlertDto(
+        this.translate('sweetalert.success'),
+        this.translate(text),
+        SweetalertType.success
+      );
+      GeneralService.sweetAlert(sweetAlertDto);
+    } else {
+      const sweetAlertDto = new SweetAlertDto(
+        this.translate('sweetalert.error'),
+        this.translate(text),
+        SweetalertType.error
+      );
+      GeneralService.sweetAlert(sweetAlertDto);
+    }
+  }
+
+  translate(key: string): any {
+    return this._translocoService.translate(key);
   }
 
 }
