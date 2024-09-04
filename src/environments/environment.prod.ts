436650760b0e690f58f77a31bcff97e4a99dd0ac
Dogan Gunes <dogangns.98@gmail.com>
2024-09-04 23:14:42 +0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5000/services/',
+    production: true,
+    apiUrl: 'http://165.22.90.165:5000/services/',
     // apiUrl: 'https://brewcloud.xyz:5000/services/',
     appName: 'vetsystems',
     env: 'dev',
