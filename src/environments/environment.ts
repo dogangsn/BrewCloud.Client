1146e1a4432d87b065f174a780bb59dc44aedb58
Dogan Gunes <dogangns.98@gmail.com>
2024-10-17 00:24:55 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:5000/services/',
     // apiUrl: 'https://brewcloud.xyz:5000/services/',
-    appName: 'vetsystems',
+    appName: 'brewcloud',
     env: 'dev',
     IsApiConnect : true
 };
