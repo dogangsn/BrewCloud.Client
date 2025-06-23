a85dc6a61d89c3d3eaba9e4150e1a127251bdcf6
Dogan Gunes <dogangns.98@gmail.com>
2025-06-24 00:33:01 +0300

    .

---
+++ src/app/modules/connecthub/dashboards/connecthubdashboards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connecthubdashboards',
+  templateUrl: './connecthubdashboards.component.html',
+  styleUrls: ['./connecthubdashboards.component.css']
+})
+export class ConnecthubdashboardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
